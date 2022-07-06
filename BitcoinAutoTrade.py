8fb0d0de811a6e2d31207c0b7c800a564e20b630
jigyoung <43769000+jigyoung@users.noreply.github.com>
2022-07-06 16:07:43 +0900

    Update BitcoinAutoTrade.py

--- BitcoinAutoTrade.py
+++ BitcoinAutoTrade.py
@@ -7,13 +7,13 @@ secret = "jYY04PYnWdtiviy8NW0cq70CJcVhxKqZWZeKXHzK"
 
 def get_target_price(ticker, k):
     """변동성 돌파 전략으로 매수 목표가 조회"""
-    df = pyupbit.get_ohlcv(ticker, interval="minute1", count=2) #Change in timeframe
+    df = pyupbit.get_ohlcv(ticker, interval="day", count=2) #Change in timeframe
     target_price = df.iloc[0]['close'] + (df.iloc[0]['high'] - df.iloc[0]['low']) * k
     return target_price
 
 def get_start_time(ticker):
     """시작 시간 조회"""
-    df = pyupbit.get_ohlcv(ticker, interval="minute1", count=1) #Change in timeframe
+    df = pyupbit.get_ohlcv(ticker, interval="day", count=1) #Change in timeframe
     start_time = df.index[0]
     return start_time
 
@@ -41,10 +41,10 @@ while True:
     try:
         now = datetime.datetime.now()
         start_time = get_start_time("KRW-BTC")
-        end_time = start_time + datetime.timedelta(minutes=1) # Change time frame
+        end_time = start_time + datetime.timedelta(days=1) # Change time frame
 
         if start_time < now < end_time - datetime.timedelta(seconds=10):
-            target_price = get_target_price("KRW-BTC", 0.7) # Change K value
+            target_price = get_target_price("KRW-BTC", 0.8) # Change K value
             current_price = get_current_price("KRW-BTC")
             if target_price < current_price:
                 krw = get_balance("KRW")
