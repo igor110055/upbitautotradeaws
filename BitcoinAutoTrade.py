292bffe9c53ef47447a926bdfe001e29ef2fdb5f
jigyoung <43769000+jigyoung@users.noreply.github.com>
2022-07-06 11:30:55 +0900

    Update BitcoinAutoTrade.py

--- BitcoinAutoTrade.py
+++ BitcoinAutoTrade.py
@@ -53,7 +53,7 @@ while True:
         else:
             btc = get_balance("BTC")
             if btc > 0.00008:
-                upbit.sell_market_order("KRW-BTC", btc*1)
+                upbit.sell_market_order("KRW-BTC", btc*1) #Change of amount
         time.sleep(1)
     except Exception as e:
         print(e)
